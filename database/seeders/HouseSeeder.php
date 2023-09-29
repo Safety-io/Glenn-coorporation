7bcc79d3734240977a81cf421aa533034e8c4eca
paul <paulkaf84@gmail.com>
2023-09-29 02:32:26 +0200

    install and config admin panel

--- database/seeders/HouseSeeder.php
+++ database/seeders/HouseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class HouseSeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class HouseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // generate 100 houses with faker
+        $faker = Factory::create();
+        for ($i = 0; $i < 100; $i++) {
+            DB::table('houses')->insert([
+                'name' => $faker->name,
+                'address' => $faker->address,
+                'rooms' => $faker->numberBetween(1, 10),
+                'bathrooms' => $faker->numberBetween(1, 10),
+                'price' => $faker->numberBetween(100000, 1000000),
+                'image' => 'houses/' . $faker->numberBetween(1, 10) . '.jpg',
+            ]);
+        }
+
     }
 }
